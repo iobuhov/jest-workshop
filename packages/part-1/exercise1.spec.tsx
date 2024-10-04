53fa080d46af269b20383e0db04fee49d7f5cd55
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2024-10-04 15:02:22 +0200

    add first exercise

---
+++ packages/part-1/exercise1.spec.tsx
@@ -0,0 +1,10 @@
+/**
+ * Task:
+ * Write a simple spec using `describe` and `it` for
+ * our `sum` function.
+ * Use "What"->"with/when<optional>"->"behavior" style to write the test.
+ */
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
